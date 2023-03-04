f789cc251c7f940db127e5f1ba25fe89e7bdc2be
M.A <md.ali2001@hotmail.com>
2023-03-04 04:17:21 -0800

    added fragments in mainactivity , but fragments not changing on clicking icons ( bottom navbar )

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ecommerce.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragcontainer)
+        val navController=navHostFragment!!.findNavController()
+
+        val popupMenu=PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.navigation.setupWithNavController(popupMenu.menu,navController)
+        navController.addOnDestinationChangedListener(object :NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+               title= when(destination.id){
+
+                   R.id.cartfragment-> "MY CART"
+                   R.id.morefragment-> "MY DASHBOARD"
+
+                   else -> "ecommerce"
+               }
+            }
+
+        })
+
+
     }
+}
+
+private fun BottomNavigationView.setupWithNavController(navController: Menu?, navController1: NavController) {
+
 }
